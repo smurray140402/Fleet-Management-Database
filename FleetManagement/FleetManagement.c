a8b1c2eea6e19c45452b20423716429e1e063253
Sean Murray <G00426904@ATU.IE>
2025-04-23 16:33:48 +0100

    Created 'menu' function and switch statement to handle user choice

--- FleetManagement/FleetManagement.c
+++ FleetManagement/FleetManagement.c
@@ -1,5 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h> 
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #define tempValue 30
 
@@ -37,8 +40,11 @@ typedef struct {
     BreakdownCategory breakdowns;
 } Machine;
 
+int menu();
 
 int main() {
+
+    int menuOption;
 
 	printf("*****************************************");
 	printf("\n*                                       *");
@@ -46,7 +52,72 @@ int main() {
 	printf("\n*                                       *");
 	printf("\n*****************************************");
 
+    // Do while loop that loops until user exits the application
+    do {
+        menuOption = menu();
+
+        // Switch to handle the users menu choice
+        switch (menuOption) {
+        case 1:
+            printf("case1");
+            break;
+        case 2:
+            printf("case2");
+            break;
+        case 3:
+            printf("case3");
+            break;
+        case 4:
+            printf("case4");
+            break;
+        case 5:
+            printf("case5");
+            break;
+        case 6:
+            printf("case6");
+            break;
+        case 7:
+            printf("case7");
+            break;
+        case 8:
+            printf("case8");
+            break;
+        default:
+            break;
+
+        }
+    } while (menuOption != 0);
+
 	return 0;
+}
+
+// Function for menu which returns the users choice
+int menu() {
+    int option;
+
+    printf("\n---------------------------");
+    printf("\n(1) ADD NEW MACHINE");
+    printf("\n---------------------------");
+    printf("\n(2) DISPLAY ALL MACHINES");
+    printf("\n---------------------------");
+    printf("\n(3) DISPLAY MACHINE DETAILS");
+    printf("\n---------------------------");
+    printf("\n(4) UPDATE A MACHINE");
+    printf("\n---------------------------");
+    printf("\n(5) DELETE A MACHINE");
+    printf("\n---------------------------");
+    printf("\n(6) GENERATE STATISTICS");
+    printf("\n---------------------------");
+    printf("\n(7) OUTPUT REPORT FILE");
+    printf("\n---------------------------");
+    printf("\n(8) CURRENT VALUATION LIST");
+    printf("\n---------------------------");
+    printf("\n(0) EXIT");
+    printf("\n---------------------------\n\n");
+
+    scanf("%d", &option);
+
+    return option;
 }
 
 

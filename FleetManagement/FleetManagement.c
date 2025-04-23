24ef04dca66bac6a78db2d4078e83847aeaaabe9
Sean Murray <G00426904@ATU.IE>
2025-04-23 16:12:21 +0100

    Define Machine struct with enums for type and breakdown categories

--- FleetManagement/FleetManagement.c
+++ FleetManagement/FleetManagement.c
@@ -1,6 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h> 
+
+#define tempValue 30
+
+// Enum that represents the different machine types
+typedef enum { 
+    TRACTOR, 
+    EXCAVATOR, 
+    ROLLER, 
+    CRANE, 
+    MIXER 
+} MachineType;
+
+// Enum that represents the categories of machine breakdowns
+typedef enum { 
+    NEVER, 
+    LESS_THAN_THREE, 
+    LESS_THAN_FIVE, 
+    MORE_THAN_FIVE 
+} BreakdownCategory;
+
+// Structure to hold all machine details
+typedef struct {
+    char chassisNumber[tempValue];
+    char make[tempValue];
+    char model[tempValue];
+    int year;
+    float cost;
+    float valuation;
+    int mileage;
+    int nextServiceMileage;
+    char ownerName[tempValue];
+    char ownerEmail[tempValue];
+    char ownerPhone[tempValue];
+    MachineType type;
+    BreakdownCategory breakdowns;
+} Machine;
+
 
 int main() {
+
+	printf("*****************************************");
+	printf("\n*                                       *");
+	printf("\n*\tFleet Management Database\t*");
+	printf("\n*                                       *");
+	printf("\n*****************************************");
 
 	return 0;
 }
